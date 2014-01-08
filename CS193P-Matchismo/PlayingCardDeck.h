0361d9763bdf5c77b523330c15174eeb154bda8a
rodrigo brancher <rbrancher@gmail.com>
2014-01-08 19:03:22 -0200

    Adds PlayingCardDeck Model

---
+++ CS193P-Matchismo/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  CS193P-Matchismo
+//
+//  Created by rodrigo brancher on 1/8/14.
+//  Copyright (c) 2014 nu. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
